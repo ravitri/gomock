eaf2dffbd5698dc940d2be614ded0c1691203882
Ravi Trivedi <travi@redhat.com>
2021-06-03 13:53:25 +0530

    Adding test file

---
+++ user/user_test.go
@@ -0,0 +1,22 @@
+package user_test
+
+import (
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/ravitri/gomock/mocks"
+	"github.com/ravitri/gomock/user"
+)
+
+func TestUse(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	mockDoer := mocks.NewMockDoer(mockCtrl)
+	testUser := &user.User{Doer: mockDoer}
+
+	// Expectt Do to be called once with 123 and "Hello GoMock" as parameters, and return nil from the mocked call.
+	mockDoer.EXPECT().DoSomething(123, "Hello GoMock").Return(nil).Times(1)
+
+	testUser.Use()
+}
